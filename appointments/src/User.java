572e81f4ed859d854230391f49342dbe99c6f1b3
Carson Barker <carsonbarker2@my.unt.edu>
2023-06-21 19:08:55 -0500

    created password variable and related functions

--- appointments/src/User.java
+++ appointments/src/User.java
@@ -3,11 +3,13 @@ public class User {
     private String name;
     private int iD;
     private String userEmail;
+    private String password;
 
-    public void user(String name, int iD, String userEmail){
+    User(String name, int iD, String userEmail, String password){
         this.name = name;
         this.iD = iD;
         this.userEmail = userEmail;
+        this.password = password;
     }
     
     public int getID(){
@@ -21,6 +23,10 @@ public class User {
     public String getEmail(){
         return userEmail;
     }
+
+    public String getPassword(){
+        return password;
+    }
     
     public void setID(int newID){
         this.iD = newID;
@@ -32,5 +38,8 @@ public class User {
 
     public void setEmail(String newEmail){
         this.userEmail = newEmail;
+    }
+    public void setPassword(String newPassword){
+        this.password = newPassword;
     }
 }
