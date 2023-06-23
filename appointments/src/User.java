414cd231e40d4e9ce2c26286981363d6d40d86e7
Carson Barker <carsonbarker2@my.unt.edu>
2023-06-23 15:02:26 -0500

    updating user and account

--- appointments/src/User.java
+++ appointments/src/User.java
@@ -1,15 +1,12 @@
-public class User {
+public class User extends Account{
     
     private String name;
     private int iD;
-    private String userEmail;
-    private String password;
+    //when appointment class is created, declare an appointment vector here
 
-    User(String name, int iD, String userEmail, String password){
+    User(String name, int iD){
         this.name = name;
         this.iD = iD;
-        this.userEmail = userEmail;
-        this.password = password;
     }
     
     public int getID(){
@@ -20,26 +17,11 @@ public class User {
         return name;
     }
 
-    public String getEmail(){
-        return userEmail;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-    
     public void setID(int newID){
         this.iD = newID;
     }
 
     public void setName(String newName){
         this.name = newName;
-    }
-
-    public void setEmail(String newEmail){
-        this.userEmail = newEmail;
-    }
-    public void setPassword(String newPassword){
-        this.password = newPassword;
     }
 }
